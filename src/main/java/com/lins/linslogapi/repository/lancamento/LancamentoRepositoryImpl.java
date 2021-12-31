02a7b614e9c1ac43cab1f773ab518ba28363f4e2
Marcelo Lins <marceloLins-code>
2021-12-31 16:39:52 -0300

    Aprimorando pesquisas com metamodel(criteria) e paginação

--- src/main/java/com/lins/linslogapi/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/lins/linslogapi/repository/lancamento/LancamentoRepositoryImpl.java
@@ -11,48 +11,76 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.hibernate.query.criteria.internal.CriteriaBuilderImpl;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.ObjectUtils;
 
 import com.lins.linslogapi.model.Lancamento;
 import com.lins.linslogapi.model.Lancamento_;
 import com.lins.linslogapi.repository.filter.LancamentoFilter;
 
-public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
-	
+public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
+
 	@PersistenceContext
 	private EntityManager manager;
 
 	@Override
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 		Root<Lancamento> root = criteria.from(Lancamento.class);
-		
+
 		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
 		criteria.where(predicates);
-		
+
 		TypedQuery<Lancamento> query = manager.createQuery(criteria);
-		return query.getResultList();
-		}
-	
-		private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
+		adicionarRestricoesDePaginacao(query, pageable);
+
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
+	}
+
+	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
 			Root<Lancamento> root) {
 		List<Predicate> predicates = new ArrayList<>();
-		
-		if(!ObjectUtils.isEmpty(lancamentoFilter.getDescricao())) {
-		predicates.add((Predicate) builder.like(
-				builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
+
+		if (!ObjectUtils.isEmpty(lancamentoFilter.getDescricao())) {
+			predicates.add((Predicate) builder.like(builder.lower(root.get(Lancamento_.descricao)),
+					"%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
 		}
 		if (lancamentoFilter.getDataVencimetoDe() != null) {
-			predicates.add((Predicate) builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimetoDe()));
+			predicates.add((Predicate) builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+					lancamentoFilter.getDataVencimetoDe()));
 		}
-		
+
 		if (lancamentoFilter.getDataVencimetoAte() != null) {
-			predicates.add(
-					(Predicate) builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimetoAte()));
+			predicates.add((Predicate) builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+					lancamentoFilter.getDataVencimetoAte()));
 		}
-		
+
 		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+	
+	//------------------------
+
+	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalDeRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistroDaPagina = paginaAtual * totalDeRegistrosPorPagina;
+		
+		query.setFirstResult(primeiroRegistroDaPagina);
+		query.setMaxResults(totalDeRegistrosPorPagina);
+
+	}
+
+	private Long total(LancamentoFilter lancamentoFilter) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		criteria.select(builder.count(root));
+		return manager.createQuery(criteria).getSingleResult();
 	}
 
 }

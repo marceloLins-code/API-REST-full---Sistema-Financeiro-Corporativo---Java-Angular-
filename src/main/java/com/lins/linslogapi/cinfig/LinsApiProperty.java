29f397590d569762cc54321af5de0405025d9ddd
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2022-03-17 16:58:39 -0300

    alteraçoes api

--- src/main/java/com/lins/linslogapi/cinfig/LinsApiProperty.java
+++ src/main/java/com/lins/linslogapi/cinfig/LinsApiProperty.java
@@ -6,19 +6,27 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("lins")
 public class LinsApiProperty {
-	
-	private final Seguranca seguranca = new Seguranca();	
+
+	private String originPermitida = "http://localhost:8000";
+
+	private final Seguranca seguranca = new Seguranca();
 
 	public Seguranca getSeguranca() {
 		return seguranca;
 	}
-	
-	
+
+	public String getOriginPermitida() {
+		return originPermitida;
+	}
+
+	public void setOriginPermitida(String originPermitida) {
+		this.originPermitida = originPermitida;
+	}
 
 	public static class Seguranca {
 
 		private boolean enableHttps;
-		
+
 		public boolean isEnableHttps() {
 			return enableHttps;
 		}
